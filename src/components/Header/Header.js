c42d1a67e679704646984cdfcb592f77b3d68fc5
Loïc <lolo62.grand@gmail.com>
2023-08-29 16:18:37 +0200

    ajout du début de la nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import styles from "./Header.module.scss";
+import stylesBtn from "../Button.scss";
+import logo from "../../assets/img/logo-jeux.png";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="divLogo">
+                <a href="">
+                    <img className={`${styles.logo}`} src={logo} alt="logo manette"></img>
+                </a>
+            </div>
+            <div className={`${styles.lienPage}`}>
+                <ul>
+                    <li><a href="">Jeux du moment</a></li>
+                    <li><a href="">Jeux à venir</a></li>
+                    <li><a href="">Jeux retro</a></li>
+                </ul>
+            </div>
+            <div className={`${styles.btnCo}`}>
+                <button className={`${stylesBtn.btnConnexion}`}>test</button>
+            </div>
+        </header>
+    )
+}
